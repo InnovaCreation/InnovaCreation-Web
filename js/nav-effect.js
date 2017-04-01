387c7ca41ad88f51c95064707453dc9281988ed8
AddamsChen <addamsone@gmail.com>
2017-04-01 22:29:46 +0800

    Beta 1.0

---
+++ js/nav-effect.js
@@ -0,0 +1,29 @@
+function GetPageScroll() 
+{ 
+    var x, y; 
+    if(window.pageYOffset) 
+    {    // all except IE    
+        y = window.pageYOffset;    
+        x = window.pageXOffset; 
+    } else if(document.documentElement && document.documentElement.scrollTop) 
+    {    // IE 6 Strict    
+        y = document.documentElement.scrollTop;    
+        x = document.documentElement.scrollLeft; 
+    } else if(document.body) 
+    {    // all other IE    
+        y = document.body.scrollTop;    
+        x = document.body.scrollLeft;   
+    } 
+ 
+    if(y>=700)
+    {
+
+        document.getElementById("nav").className = "ic-nav-down";
+        //id输入替换nav
+    }
+    else
+    {
+        document.getElementById("nav").className = "ic-nav-up";
+        //id输入替换nav
+    }
+}
